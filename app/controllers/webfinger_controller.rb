07412ec49c0969a735beb14ec98c9ba85a0729d5
Rexmortus the Red <rexmortus@192-168-1-103.tpgi.com.au>
2019-11-06 16:55:01 +1100

    Spoofed webfinger

---
+++ app/controllers/webfinger_controller.rb
@@ -0,0 +1,26 @@
+class WebfingerController < ApplicationController
+
+  def account
+
+    username = params[:resource].match('(?<=\:).+?(?=\@)').to_s
+    # binding.pry
+
+    if User.where(username: username).exists?(conditions = :none)
+      response = {
+        "subject": params[:resource],
+        "links": [
+          {
+            "rel": "self",
+            "type": "application/activity+json",
+            "href": "#{root_url}actor"
+          }
+        ]
+      }
+      render :json => response.to_json
+    else
+      render :json => {'error': 'doesnt exist'}.to_json
+    end
+
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'admin@classifeds.aus.social', username: 'admin', password: 'topsecret', password_confirmation: 'topsecret')
