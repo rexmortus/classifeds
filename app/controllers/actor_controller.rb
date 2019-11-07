e771c4f14e6e1a2e2bf552f4216f342882c343a6
Rexmortus the Red <rexmortus@192-168-1-103.tpgi.com.au>
2019-11-07 22:38:57 +1100

    Fix route

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -4,13 +4,43 @@ class ActorController < ApplicationController
 
   # show an activitypub actor for a given username
   def show_actor_for_user
-    user = User.find_by username: params[:username]
-    render :json => user.actor
+    username = params[:username]
+    user = User.find_by username: username
+
+    if user.present?
+      render :json => user.actor
+    else
+      render json: {'error': "User #{username} found."}, status: 400
+    end
   end
 
   # the inbox for a individual activitypub actor
   def inbox_for_actor
-    body = JSON.parse(request.body.read)
-    logger.info body
+
+    username = params[:username]
+    user = User.find_by username: username
+
+    if user.present?
+      body = JSON.parse(request.body.read)
+      actor = JSON.parse(user.actor)
+
+      case body["type"]
+      when "Follow"
+        accept_message_body = {
+          "@context": "https://www.w3.org/ns/activitystreams",
+          "id": "#{ENV['ROOT_CLASSIFEDS_URL']}#{SecureRandom.uuid}",
+          "type": "Accept",
+          "actor": "#{user.actor["id"]}",
+          "object": body
+        }
+        logger.info accept_message_body
+      else
+        render json: {'error', "#{body['type']} not supported"}, status: 500
+      end
+    else
+      render json: {'error': "User #{username} found."}, status: 400
+    end
+
+
   end
 end
