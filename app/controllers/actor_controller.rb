d52a7878a6f77bd1d53f78574695cb672fbd8182
Rexmortus the Red <rexmortus@192-168-1-103.tpgi.com.au>
2019-11-07 23:27:15 +1100

    Fix route

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -26,14 +26,8 @@ class ActorController < ApplicationController
 
       case body["type"]
       when "Follow"
-        accept_message_body = {
-          "@context": "https://www.w3.org/ns/activitystreams",
-          "id": "#{ENV['ROOT_CLASSIFEDS_URL']}#{SecureRandom.uuid}",
-          "type": "Accept",
-          "actor": "#{actor['id']}",
-          "object": body
-        }
-        logger.info accept_message_body
+        accept_message_body = generate_accept_message(actor["id"], body)
+        send_accept_message(accept_message_body, user)
       else
         render json: {'error': "Action not supported."}, status: 400
       end
@@ -43,4 +37,33 @@ class ActorController < ApplicationController
 
 
   end
+
+  private
+
+  def generate_accept_message(actor_id, body)
+    {
+      "@context": "https://www.w3.org/ns/activitystreams",
+      "id": "#{ENV['ROOT_CLASSIFEDS_URL']}#{SecureRandom.uuid}",
+      "type": "Accept",
+      "actor": "#{actor_id}",
+      "object": body
+    }
+  end
+
+  def send_accept_message(accept_message_body, user)
+
+    logger.info 'Sending accept message...'
+    time = Time.now.utc.to_s
+
+    inboxFragment = "something"
+    targetDomain = "some.domain"
+    string_to_sign = "(request-target): post #{inboxFragment}\nhost: #{targetDomain}\ndate: #{time}`;"
+    digest = OpenSSL::Digest.new('sha256')
+    signature = OpenSSL::HMAC.hexdigest(digest, user.privkey, string_to_sign)
+    signature_b64 = Base64.strict_encode64(signature)
+
+    header = "keyId=\"#{accept_message_body["id"]}\",headers=\"(request-target) host date\",signature=\"#{signature_b64}\"";
+    logger.info header
+  end
+
 end
