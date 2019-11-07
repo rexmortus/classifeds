a3ce4fc2c4da92d1fbfe243d349a9d7b3d5c0d52
Rexmortus the Red <rexmortus@192-168-1-103.tpgi.com.au>
2019-11-07 22:43:06 +1100

    Fix route

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -30,15 +30,15 @@ class ActorController < ApplicationController
           "@context": "https://www.w3.org/ns/activitystreams",
           "id": "#{ENV['ROOT_CLASSIFEDS_URL']}#{SecureRandom.uuid}",
           "type": "Accept",
-          "actor": "#{user.actor["id"]}",
+          "actor": "#{user.actor['id']}",
           "object": body
         }
         logger.info accept_message_body
       else
-        render json: {'error', "#{body['type']} not supported"}, status: 500
+        render json: {'error': "Action not supported."}, status: 400
       end
     else
-      render json: {'error': "User #{username} found."}, status: 400
+      render json: {'error': "User #{username} not found."}, status: 400
     end
 
 
