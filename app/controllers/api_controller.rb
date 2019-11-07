b3b92a4f1f52bd224f7e587c3929e188637e30f0
Rexmortus the Red <rexmortus@192-168-1-103.tpgi.com.au>
2019-11-07 13:34:25 +1100

    Whoopsie

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
 
-  skip_before_action :authenticate_user, :verify_authenticity_token
+  skip_before_action :authenticate_user!, :verify_authenticity_token
 
   def inbox
     logger.debug params.as_json.to_s
