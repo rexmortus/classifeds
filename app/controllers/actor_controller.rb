e171ef186c606f0872cec8d6e3c1386c3770435a
Rexmortus the Red <rexmortus@192-168-1-103.tpgi.com.au>
2019-11-08 20:13:55 +1100

    Fix route

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -21,15 +21,20 @@ class ActorController < ApplicationController
     user = User.find_by username: username
 
     if user.present?
-      body = JSON.parse(request.body.read)
-      actor = JSON.parse(user.actor)
+      # The local actor, receiving the action
+      local_actor = JSON.parse(user.actor)
 
-      logger.info body["type"]
+      # The remote action
+      remote_action = JSON.parse(request.body.read)
 
-      case body["type"]
+      # The remote actor, taking the action
+      remote_actor = sender_action["actor"]
+
+      # Generate a response message based on action type
+      case remote_action["type"]
       when "Follow"
-        message = generate_accept_message(actor, body)
-        send_accept_message(message, user, actor)
+        accept_message = generate_accept_message(local_actor, remote_action)
+        send_accept_message(accept_message, local_actor, remote_actor)
       else
         render json: {'error': "Action not supported."}, status: 400
       end
@@ -42,29 +47,24 @@ class ActorController < ApplicationController
 
   private
 
-  def generate_accept_message(actor, body)
-    logger.info 'START generating accept message body...'
-    message = {
+  # Generate an 'Accept' message on behalf of the receiving actor
+  def generate_accept_message(local_actor, sender_action)
+    return {
       "@context": "https://www.w3.org/ns/activitystreams",
       "id": "#{ENV['ROOT_CLASSIFEDS_URL']}#{SecureRandom.uuid}",
       "type": "Accept",
-      "actor": "#{actor['id']}",
-      "object": body
+      "actor": "#{local_actor["id"]}",
+      "object": sender_action
     }
-    logger.info message
-    logger.info 'DONE generating accept message body...'
-    return message
   end
 
-  def send_accept_message(message, user, actor)
-
-    logger.info 'STARTED Sending accept message...'
-    logger.info message
+  def send_accept_message(accept_message, local_actor, remote_actor)
 
     time = Time.now.utc.to_s
-    inboxFragment = "something"
-    targetDomain = "some.domain"
-    string_to_sign = "(request-target): post #{inboxFragment}\nhost: #{targetDomain}\ndate: #{time}`;"
+    inboxPath = "#{remote_actor}/inbox"
+    targetDomain = URI(remote_actor).host
+    string_to_sign = "(request-target): post #{inboxPath}\nhost: #{targetDomain}\ndate: #{time}"
+    logger.info string_to_sign
     digest = OpenSSL::Digest.new('sha256')
     signature = OpenSSL::HMAC.hexdigest(digest, user.privkey, string_to_sign)
     signature_b64 = Base64.strict_encode64(signature)
