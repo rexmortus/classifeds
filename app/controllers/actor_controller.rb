6c9e3f9908bfa9932e464937917777fc2d70ac59
Rexmortus the Red <rexmortus@192-168-1-103.tpgi.com.au>
2019-11-11 13:34:05 +1100

    Fix route

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -37,15 +37,10 @@ class ActorController < ApplicationController
       # }
       remote_action = JSON.parse(request.body.read)
 
-      # The remote actor, taking the action (a string), e.g.
-      # https://aus.social/users/rexmortus
-      remote_actor = remote_action["actor"]
-
       # Generate a response message based on action type
       case remote_action["type"]
       when "Follow"
-        accept_message = generate_accept_message(local_actor, remote_action)
-        send_accept_message(accept_message, user, local_actor, remote_actor)
+        AcceptFollowJob.perform(user, local_actor, remote_action)
       else
         render json: {'error': "Action not supported."}, status: 400
       end
@@ -53,44 +48,6 @@ class ActorController < ApplicationController
       render json: {'error': "User #{username} not found."}, status: 400
     end
 
-
-  end
-
-  private
-
-  # Generate an 'Accept' message on behalf of the receiving actor
-  def generate_accept_message(local_actor, remote_action)
-    return {
-      "@context": "https://www.w3.org/ns/activitystreams",
-      "id": "#{ENV['ROOT_CLASSIFEDS_URL']}#{SecureRandom.uuid}",
-      "type": "Accept",
-      "actor": "#{local_actor["id"]}",
-      "object": remote_action
-    }
-  end
-
-  def send_accept_message(accept_message, user, local_actor, remote_actor)
-
-    date            = Time.now.utc.httpdate
-    inboxPath       = "#{URI(remote_actor).path}/inbox"
-    targetDomain    = URI(remote_actor).host
-    signed_string  = "(request-target)s: post #{inboxPath}\nhost: #{targetDomain}\ndate: #{date}"
-    keypair         = OpenSSL::PKey::RSA.new(user.privkey)
-    signature       = Base64.strict_encode64(keypair.sign(OpenSSL::Digest::SHA256.new, signed_string))
-
-    header          = "keyId=\"#{local_actor['id']}\",headers=\"(request-target) host date\",signature=\"#{signature}\"";
-
-    response = HTTP.headers(
-      {
-        'Host': targetDomain,
-        'Date': date,
-        'Signature': header
-      }
-    )
-    .post("#{remote_actor}/inbox", body: accept_message.to_json)
-
-    logger.info "Response from #{targetDomain}"
-    logger.info response.status
 
   end
 
---
+++ app/jobs/accept_follow_job.rb
@@ -0,0 +1,45 @@
+class AcceptFollowJob < ApplicationJob
+  queue_as :default
+
+  def perform(user, local_actor, remote_action)
+    accept_message = generate_accept_message(local_actor, remote_action)
+    response = send_accept_message(accept_message, user, local_actor, remote_action["actor"])
+    logger.info 'RESPONSE FROM SERVER'
+    logger.info response.status
+  end
+
+  private
+
+  # Generate an 'Accept' message on behalf of the receiving actor
+  def generate_accept_message(local_actor, remote_action)
+    return {
+      "@context": "https://www.w3.org/ns/activitystreams",
+      "id": "#{ENV['ROOT_CLASSIFEDS_URL']}#{SecureRandom.uuid}",
+      "type": "Accept",
+      "actor": "#{local_actor["id"]}",
+      "object": remote_action
+    }
+  end
+
+  def send_accept_message(accept_message, user, local_actor, remote_actor)
+
+    date            = Time.now.utc.httpdate
+    inboxPath       = "#{URI(remote_actor).path}/inbox"
+    targetDomain    = URI(remote_actor).host
+    signed_string   = "(request-target): post #{inboxPath}\nhost: #{targetDomain}\ndate: #{date}"
+    keypair         = OpenSSL::PKey::RSA.new(user.privkey)
+    signature       = Base64.strict_encode64(keypair.sign(OpenSSL::Digest::SHA256.new, signed_string))
+
+    header          = "keyId=\"#{local_actor['id']}\",headers=\"(request-target) host date\",signature=\"#{signature}\"";
+
+    response = HTTP[
+      :Host => targetDomain,
+      :Date => date,
+      :Signature' => header
+    ]
+    .post("#{remote_actor}/inbox", body: accept_message.to_json)
+
+    return response
+
+  end
+end
