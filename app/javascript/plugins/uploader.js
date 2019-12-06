3cc144497dabe5e17350d829acd56f9ddec7ebda
Rexmortus the Red <rexmortus@192-168-1-103.tpgi.com.au>
2019-12-07 07:19:34 +1100

    Fix simple image preview

--- app/javascript/plugins/uploader.js
+++ app/javascript/plugins/uploader.js
@@ -2,43 +2,44 @@ function previewImages() {
 
   var preview = document.getElementById('image-preview');
 
-  if (preview) {
-    preview.innerHTML = '';
+  preview.innerHTML = '';
 
-    if (this.files) {
-      [].forEach.call(this.files, readAndPreview);
-    }
+  if (this.files) {
+    [].forEach.call(this.files, readAndPreview);
+  }
 
-    function readAndPreview(file) {
+  function readAndPreview(file) {
 
-      if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
-        return alert(file.name + " is not an image");
-      } // else...
+    if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
+      return alert(file.name + " is not an image");
+    } // else...
 
-      var reader = new FileReader();
+    var reader = new FileReader();
 
-      reader.addEventListener("load", function() {
+    reader.addEventListener("load", function() {
 
-        const div = document.createElement('div');
-        div.classList.add('image-container');
-        div.classList.add('js-reveal-photo');
+      const div = document.createElement('div');
+      div.classList.add('image-container');
+      div.classList.add('js-reveal-photo');
 
-        var image = new Image();
-        image.height = 300;
-        image.title  = file.name;
-        image.src    = this.result;
+      var image = new Image();
+      image.height = 300;
+      image.title  = file.name;
+      image.src    = this.result;
 
-        div.appendChild(image);
+      div.appendChild(image);
 
-        preview.appendChild(div);
-      });
+      preview.appendChild(div);
+    });
 
-      reader.readAsDataURL(file);
-
-    }
-
-    document.querySelector('#advertisement_images').addEventListener("change", previewImages);
+    reader.readAsDataURL(file);
 
   }
 
+}
+
+const image_input = document.getElementById('advertisement_images');
+
+if (image_input) {
+    image_input.addEventListener("change", previewImages);
 }
