3ab01912ab11cd4400a1003f4c2a343b0cb44e3a
Allen Liao <a@tzukuri.com>
2019-12-09 15:41:19 +1100

    altered some of the search queries

--- app/controllers/v2_search_controller.rb
+++ app/controllers/v2_search_controller.rb
@@ -1,13 +1,16 @@
 class V2SearchController < ApplicationController
   skip_before_action :authenticate_user!, only: [:search]
   def search
+    @advertisements = filter(Advertisement.all)
   end
 
   private
   def filter(ads)
-    ads = Advertisement.where("title ILIKE (?) OR body ILIKE (?)", "%#{params["query"]}%", "%#{params["query"]}%") if exists?(params[:query])
-    ads = Advertisement.near(params["location"], params["distance"], units: :km) if exists?(params["location"])
-    ads = Advertisement.where("category = ? OR subcategory = ?", params["category"], params["category"])
+    ads = ads.where("title ILIKE (?) OR body ILIKE (?)", "%#{params["query"]}%", "%#{params["query"]}%") if exists?(params[:query])
+    ads = ads.near(params["location"], params["distance"], units: :km) if exists?(params["location"])
+    ads = ads.where("category_name = ? OR subcategory_name = ?", params["category"], params["category"]) if exists?(params["category"])
+    ads = ads.where("for = ?", params["for"]) if exists?(params["for"])
+    return ads
   end
   # Check if parameter is supplied for filtering or search
   def exists?(parameter)
