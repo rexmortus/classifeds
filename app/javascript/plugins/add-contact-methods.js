ef9706e7e21fdd824cd58d79662ce279ab9cd151
Rexmortus the Red <rexmortus@192-168-1-103.tpgi.com.au>
2019-12-24 09:57:34 +1100

    Better UI for contact methods

--- app/javascript/plugins/add-contact-methods.js
+++ app/javascript/plugins/add-contact-methods.js
@@ -10,24 +10,65 @@ if (editContactMethods) {
 
     event.preventDefault();
 
+    // Get the table body (this sucks)
+    let tbody = editContactMethods.children[0].children[0].children[1];
+
     // Get the index of the new contact method we're adding
-    let newIndex = editContactMethods.firstElementChild.childElementCount + 1;
+    let newIndex = tbody.childElementCount + 1;
 
-    // Add a new input to the names column
-    let nameColumn = editContactMethods.firstElementChild;
-    let newNameInput = document.createElement('input');
-    newNameInput.type = "text";
-    newNameInput.name = "user[contact_methods][" + newIndex + "][name]";
-    newNameInput.tabIndex = newIndex + 2;
-    nameColumn.appendChild(newNameInput);
+    // Create a table row
+    let tableRow = document.createElement('tr');
 
-    // Add a new input to the values column
-    let valueColumn = editContactMethods.lastElementChild;
+    // CREATE THE TYPE COLUMN/INPUT
+    let typeColumn = document.createElement('td');
+
+    // Create the input
+    let newTypeInput = document.createElement('input');
+    newTypeInput.type = "text";
+    newTypeInput.name = "user[contact_methods][" + newIndex + "][name]";
+    newTypeInput.tabIndex = newIndex + 2;
+
+    // Put the input in the column
+    typeColumn.appendChild(newTypeInput);
+
+    // Put the column into the new row
+    tableRow.appendChild(typeColumn);
+
+    tbody.appendChild(tableRow);
+
+    // CREATE THE VALUE COLUMN/INPUT
+    let valueColumn = document.createElement('td');
+
+    // Create the input
     let newValueInput = document.createElement('input');
     newValueInput.type = "text";
     newValueInput.name = "user[contact_methods][" + newIndex + "][value]";
     newValueInput.tabIndex = newIndex + 2;
+
+    // Put the input in the column
     valueColumn.appendChild(newValueInput);
+
+    // Put the column into the new row
+    tableRow.appendChild(valueColumn);
+
+    // CREATE THE PUBLIC COLUMN/INPUT
+    let publicColumn = document.createElement('td');
+    publicColumn.className = "public-input";
+
+    // Create the input
+    let newPublicInput = document.createElement('input');
+    newPublicInput.type = "checkbox";
+    newPublicInput.name = "user[contact_methods][" + newIndex + "][public]";
+
+    // Put the input in the column
+    publicColumn.appendChild(newPublicInput);
+
+    // Put the column into the new row
+    tableRow.appendChild(publicColumn);
+
+    // FINALLY, ADD THE NEW ROW
+    tbody.appendChild(tableRow);
+
 
   })
 
